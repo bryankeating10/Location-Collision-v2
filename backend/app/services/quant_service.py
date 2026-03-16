918435459756c8b9fc0713a8bd8d77c3008a606b
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-16 17:21:45 -0400

    create_quant function

---
+++ backend/app/services/quant_service.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Session
+from app.db.models import Quant
+
+def create_quant(db: Session, name: str):
+    quant = Quant(name=name)
+    db.add(quant)
+    db.commit()
+    db.refresh(quant)
+    return quant
