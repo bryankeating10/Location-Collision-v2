2aa254e6f1525eef0823e7f17c7cecf5a5bdff28
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-16 17:26:41 -0400

    Uses service functions

--- backend/app/api/quants.py
+++ backend/app/api/quants.py
@@ -1,18 +1,14 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.db.db import get_session
-from app.db.models import Quant
+from app.services.quant_service import create_quant, get_quants
 
 router = APIRouter(prefix="/quants", tags=["quants"])
 
 @router.post("/")
-def create_quant(name:str, session: Session = Depends(get_session)):
-    quant = Quant(name=name)
-    session.add(quant)
-    session.commit()
-    session.refresh(quant)
-    return quant
+def create_quant(name:str, db: Session = Depends(get_session)):
+    return create_quant(db=db, name=name)
 
 @router.get("/")
-def list_quants(session: Session = Depends(get_session)):
-    return session.query(Quant).all()
+def list_quants(db: Session = Depends(get_session)):
+    return get_quants(db=db)
