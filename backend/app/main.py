858c4fbc7093189b19205d4639a2606aa89c3da0
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-16 17:45:53 -0400

    Added API routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI
 from app.db.db import engine, Base
 from app.db import models
 
+from app.api import quants, testers, casinos, accounts, locations, actions
+
 app = FastAPI()
 
-print(Base.metadata.tables)
-
 Base.metadata.create_all(bind=engine)
+
+# Include API endpoint routers
+app.include_router(quants.router)
+app.include_router(testers.router)
+app.include_router(casinos.router)
+app.include_router(accounts.router)
+app.include_router(locations.router)
+app.include_router(actions.router)
+
 
 @app.get("/")
 def root():
