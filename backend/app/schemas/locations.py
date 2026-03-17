e71dbf63b7a6d828906f99697538dd0d882f7def
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-17 10:53:29 -0400

    Optional class use to fix None error

--- backend/app/schemas/locations.py
+++ backend/app/schemas/locations.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 
 class LocationResponse(BaseModel):
     id: int
     name: str
     latitude: float
     longitude: float
+    updated_at: Optional[datetime] = None
     created_at: datetime
 
     class Config:
