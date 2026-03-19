17a98ef5c648e02831c96a04368d1129d88ffebb
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-19 04:06:40 -0400

    Create and update schemas

--- backend/app/schemas/casinos.py
+++ backend/app/schemas/casinos.py
@@ -1,10 +1,31 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+class CasinoCreate(BaseModel):
+    name: str
+    network: str
+    active: bool
+    signup_rest: bool
+    deposit_rest: bool
+    play_rest: bool
+    withdrawal_rest: bool
+    network_rest: bool
+
+class CasinoUpdate(BaseModel):
+    name: str
+    network: str
+    active: bool
+    signup_rest: bool
+    deposit_rest: bool
+    play_rest: bool
+    withdrawal_rest: bool
+    network_rest: bool
+
 class CasinoResponse(BaseModel):
     id: int
     name: str
     network: str
+    active: bool
     signup_rest: bool
     deposit_rest: bool
     play_rest: bool
