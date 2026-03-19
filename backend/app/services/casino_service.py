0cc41f580d6d0f96122b9ee16410a79748b547be
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-19 04:13:45 -0400

    Created update service function
    Thats such a confusing commit message

--- backend/app/services/casino_service.py
+++ backend/app/services/casino_service.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from app.db.models import Casino
+from app.schemas.casinos import CasinoCreate, CasinoUpdate, CasinoResponse
 
 def create_casino(name: str, network: str, active: bool, signup_rest: bool, \
                   deposit_rest: bool, play_rest: bool, withdrawal_rest: bool, \
@@ -12,6 +13,17 @@ def create_casino(name: str, network: str, active: bool, signup_rest: bool, \
     db.commit()
     db.refresh(casino)
     return casino
+
+def update_casino(id: int, casino_data: CasinoUpdate, db: Session):
+    casino = db.query(Casino).where(Casino.id == id).first()
+    if not casino:
+        return None
+    for field, value in casino_data.model_dump().items():
+        setattr(casino, field, value)
+    db.add(casino)
+    db.commit()
+    return casino
+
 
 def list_casinos(db: Session):
     return db.query(Casino).all()
