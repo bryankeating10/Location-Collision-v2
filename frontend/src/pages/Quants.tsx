34616da5328e61f438b8703c9e030e4a47270366
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-22 12:39:42 -0400

    Placeholder pages

---
+++ frontend/src/pages/Quants.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Quants: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold">Quants Page (Coming Soon!)</h1>
+    </div>
+  );
+};
+
+export default Quants;
