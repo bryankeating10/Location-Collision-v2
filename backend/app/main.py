9f9d7f571b8d29800c0431e9db8aef2a87f86921
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-22 11:59:29 -0400

    Backend complete
    For now

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from db.db import engine, Base
-from db import models
+from .db.db import engine, Base
+from .db import models
 
-from api import quants, testers, casinos, accounts, locations, actions
+from .api import quants, testers, casinos, accounts, locations, actions
 
 from fastapi.middleware.cors import CORSMiddleware
 
